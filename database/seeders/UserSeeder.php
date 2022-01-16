a11fe0b29cb255abdf5f364ee627208203dfda62
AdriCQ <adrian.capote95@zoho.com>
2022-01-16 00:57:46 -0500

    Update password crypt

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,19 +7,19 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::query()->insert([
-            'name' => 'Admin',
-            'email' => 'email@email.com',
-            'phone' => '555555',
-            'password' => bcrypt('password'),
-            'type' => 'ADMIN'
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    User::query()->insert([
+      'name' => 'Admin',
+      'email' => 'email@email.com',
+      'phone' => '555555',
+      'password' => bcrypt('b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'),
+      'type' => 'ADMIN'
+    ]);
+  }
 }
