15fa0024d9527072a3832d99dcf1628ed8fe6775
AdriCQ <adrian.capote95@zoho.com>
2022-01-19 14:42:47 -0500

    Favicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/jpg" href="{{ asset('favicon.ico') }}"/>
     <title>@if (isset($config) && isset($config['name']))
         {{ $config['name'] }}
     @endif</title>
