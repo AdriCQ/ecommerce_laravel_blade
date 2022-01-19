c30ab3d2ef0b543f3a2164540ef1b7d3fe9a94a5
AdriCQ <adrian.capote95@zoho.com>
2022-01-19 08:25:02 -0500

    Style links

--- resources/views/components/widget/single-product.blade.php
+++ resources/views/components/widget/single-product.blade.php
@@ -3,13 +3,13 @@
         <img src="{{ $product['image'] }}" alt="{{ $product['name'] }}" class="product-thumb">
     </a>
     <h2><a  href="{{ route('product-details', ['id'=>$product['id']]) }}">{{ $product['name'] }}</a></h2>
-    <div class="product-wid-rating">
+    {{-- <div class="product-wid-rating">
         <i class="fa fa-star"></i>
         <i class="fa fa-star"></i>
         <i class="fa fa-star"></i>
         <i class="fa fa-star"></i>
         <i class="fa fa-star"></i>
-    </div>
+    </div> --}}
     <div class="product-wid-price">
         <ins>${{ $product['price'] }}.00</ins>
         <div class="btn c-primary btn-sm" onclick="shopCartHelper().addProductCart({{json_encode($product)}},1)">
